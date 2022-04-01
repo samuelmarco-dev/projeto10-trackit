f43ab2ff32fda38e9dae55798c38b6cfc23fdc22
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-04-01 11:20:49 -0300

    marcando e desmarcando hábitos

--- src/components/TelaHoje/index.js
+++ src/components/TelaHoje/index.js
@@ -6,7 +6,7 @@ import Footer from '../Footer';
 
 import { Container } from "./style";
 
-import { getHabitosHoje } from '../services/dataAxios';
+import { getHabitosHoje, postHabitoFeito, postDesmarcarHabitoFeito } from '../services/dataAxios';
 import ContextToken from '../contexts/Token';
 
 import { ImCheckboxChecked } from 'react-icons/im';
@@ -23,10 +23,7 @@ function retornaData(){
 
 function HabitosHoje() {
     const { tokenUsuario } = useContext(ContextToken);
-    console.log(tokenUsuario);
-
     const tokenLocal = localStorage.getItem('token');
-    console.log(tokenLocal);
 
     const [habitosHoje, setHabitosHoje] = useState([]);
 
@@ -55,16 +52,68 @@ function HabitosHoje() {
                 swal('Erro ao listar os seus hábitos!');
             });
         }
-        const promise = getHabitosHoje(tokenLocal);
-        promise.then(response => {
-            console.log(response.data);
-            setHabitosHoje(response.data);
-        }).catch(error => {
-            console.log(error);
-        });
     }
 
     console.log(habitosHoje);
+
+    function toggleHabito(habito){
+        const {done} = habito;
+        if(done === true){
+            return <ImCheckboxChecked className={'icon concluido'} onClick={()=>desmarcarHabitosFeito(habito)} />
+        }
+        if(done === false){
+            return <ImCheckboxChecked className={'icon'} onClick={()=>marcarHabitosFeito(habito)} />
+        }
+    }
+    
+    function marcarHabitosFeito (habitoClicado){
+        const { id } = habitoClicado;
+        
+        if(tokenUsuario !== null) {
+            const promise = postHabitoFeito(id, tokenUsuario, {done: true});
+            promise.then(response => {
+                console.log(response.data);
+                listarHabitosHoje();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+        }else{
+            const promise = postHabitoFeito(id, tokenLocal, {done: true});
+            promise.then(response => {
+                console.log(response.data);
+                listarHabitosHoje();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+        }
+    }
+    
+    function desmarcarHabitosFeito (habitoClicado){
+        const { id } = habitoClicado;
+
+        if(tokenUsuario !== null) {
+            const promise = postDesmarcarHabitoFeito(id, tokenUsuario, {done: false});
+            promise.then(response => {
+                console.log(response.data);
+                listarHabitosHoje();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+        }
+        else{
+            const promise = postDesmarcarHabitoFeito(id, tokenLocal, {done: false});
+            promise.then(response => {
+                console.log(response.data);
+                listarHabitosHoje();
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
+        }
+    }
 
     return ( 
         <Container>
@@ -76,7 +125,7 @@ function HabitosHoje() {
                 {habitosHoje.length > 0 ?
                     // eslint-disable-next-line array-callback-return
                     habitosHoje.map((habito) => {
-                        const { id, name, done, currentSequence, highestSequence } = habito;
+                        const { id, name, currentSequence, highestSequence } = habito;
                         return (
                             <div className='painel-habitosHoje' key={id}>
                                 <div className="descricao">
@@ -84,16 +133,16 @@ function HabitosHoje() {
                                     {currentSequence > 0 ? 
                                         <Paragrafo classe="sequencia" conteudo={`Sequência atual: ${currentSequence} dia(s)`} />
                                     : 
-                                        <Paragrafo classe="sequencia" conteudo={"Sequência atual: Ainda não há!"} />
+                                        <Paragrafo classe="sequencia" conteudo={"Sequência atual: Ainda não!"} />
                                     }
                                     {highestSequence > 0 ? 
                                         <Paragrafo classe="recorde" conteudo={`Seu recorde: ${highestSequence} dia(s)`} />
                                     : 
-                                        <Paragrafo classe="recorde" conteudo={"Seu recorde: Ainda não há!"} />
+                                        <Paragrafo classe="recorde" conteudo={"Seu recorde: Ainda não!"} />
                                     }
                                 </div>
                                 <figure>
-                                    <ImCheckboxChecked className={done === true ? 'icon' : 'icon selecionado'}/>
+                                    {toggleHabito(habito)}
                                 </figure>
                             </div>
                         )
